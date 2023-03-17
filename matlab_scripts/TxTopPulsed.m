903363fe94c96bf4864e42f5823f5d69d00a1587
Leandro <leoborgnino@gmail.com>
2023-03-17 17:24:47 -0300

    Agregado Tx lidar pulsado

---
+++ matlab_scripts/TxTopPulsed.m
@@ -0,0 +1,27 @@
+classdef TxTopPulsed
+    %TXTOPPULSED Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        SettingsTxPulsed;
+    end
+    
+    methods
+        function obj = TxTopPulsed(SettingsTxPulsed)
+            obj.SettingsTxPulsed  = SettingsTxPulsed;
+        end
+        
+        function outputTxPulsed = ProcessTx(obj, tline, FS, PLOT_TX)
+            L_pulso = obj.SettingsTxPulsed.TAU_SIGNAL*FS;
+            x_t = [zeros(floor((length(tline)-L_pulso)/2),1);sqrt(obj.SettingsTxPulsed.PTX).*ones(L_pulso,1);zeros(floor((length(tline)-L_pulso)/2)+1,1)];
+            length(x_t)
+            length(tline)
+            if (PLOT_TX)
+               figure
+               plot(tline,x_t)
+            end
+            outputTxPulsed = x_t;
+        end
+    end
+end
+
--- matlab_scripts/config.m
+++ matlab_scripts/config.m
@@ -8,6 +8,13 @@ SettingsTx = struct('PTX'        ,           50e-3, ... % Watts
                     'Q_ELECT'    ,         1.6e-19, ... %C
                     'T_MEAS'     ,            5e-6 ... %s
                    )
+
+SettingsTxPulsed = struct('PTX'        ,           50e-3, ... % Watts
+                          'TAU_SIGNAL' ,            5e-9, ... % s
+                          'LAMBDA0'    ,          950e-9, ... % m
+                          'Q_ELECT'    ,         1.6e-19 ...  % C
+                          )
+
 %%% Receptor Rx
 SettingsRx = struct( 'ARX'    , pi*(2.54e-2/2)^2, ... % 1 in de diametro Apertura del receptor
                      'LAMBDA0',          1550e-9, ... % m
